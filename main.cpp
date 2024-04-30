5dec9ea0ca11baa9589c3ac3afe5c658c523871a
Hussein-249 <husseinadi8@gmail.com>
2024-04-30 10:33:58 -0400

    Some tinkering

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "math/karatsuba.h"
+
 using namespace std;
 
 int main()
--- math/karatsuba.cpp
+++ math/karatsuba.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <tuple>
 #include "karatsuba.h"
 
@@ -7,22 +8,30 @@ int karatsuba() { return 0; }
 
 template <typename T, typename... Args>
 typename std::enable_if<std::is_arithmetic<T>::value, int>::type
-karatsuba(T start, Args... args)
-{
-    // int result = start;
+karatsuba(T start, Args... args) {
+    std::size_t numArgs = sizeof...(args);
 
-    if constexpr(sizeof...(args) > 0) {
+    if (numArgs > 0) {
 
         auto argtuple = std::make_tuple(args...);
 
         auto firstArgument = std::get<0>(argtuple);
+
         // auto firstArgument = std::get<0>(std::tuple<Args...>(args)); // apparently C++ doesn't like this
-    }
+
+        if (numArgs == 1) {
+
+            std::string a = std::to_string(start);
+            std::string b = std::to_string(firstArgument);
+
+            short lena = a.length();
+            short lenb = b.length();
+
+        }
+    } else { return start; }
 
     // determine m for both start and firstargument
     // splt into two and call recursively
 
-    // make base case for 2
-
-
+    // make base case for two
 }
